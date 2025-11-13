f972a814db75ae69039e62f47424547bd2420ae8
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-11-13 12:06:36 +0300

    waxaan ku soo daray icon, contact social media

--- src/config/site.js
+++ src/config/site.js
@@ -1,10 +1,12 @@
 export const site = {
   brand: "Faatixa",
   email: "hello@faatixa.com",
-  phone: "+252-616739858",
-  whatsapp: "252616739858",
+  phone: "+252 613103645",
+  whatsapp: "252613103645",
   socials: {
-    twitter: "#",
+    twitter: "https://x.com/Faatixa257582",
+    instagram: "https://www.instagram.com/faatixa.so/",
+    tiktok: "https://www.tiktok.com/@faatixa.cmc?_r=1&_t=ZM-91MP7OaRU4W",
     linkedin: "#"
   }
 }
